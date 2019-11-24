c8acb04ca97ce7d9ef46188ef0fee00e5112b8ae
Ivory Sherman <ivorysherman@Ivorys-MacBook-Pro.local>
2019-11-23 21:36:33 -0500

    multiple params WIP

--- me.js
+++ me.js
@@ -1,23 +1,59 @@
-$('.cnnix-cards__filter-buttons-wrapper').on('click', 'button', function() {
-    let filterValue = $(this).attr('data-filter');
-    let sortValue = $(this).attr('data-sort');
-    //console.log(sortValue);
+(function(scs) {
+    scs(window.jQuery, window, document);
+    }(function($, window, document) {
 
-    $cardSort.isotope({ 
-      filter: filterValue, 
-      sortBy: sortValue, 
-      sortAscending: true
-    });
+        'use strict';
 
-  //hide/show glossary based on card filterValue
-  if (filterValue === '*') {
-      $('.card').hide();
-  } else {
-      $('.card').hide();
-      $('.card' + filterValue).show();
-  }
+        function getDogImage(userBreedEntry) {
+            
+            //handle use cases if user input capitalizes any letter in input
+            let breedParameter = userBreedEntry.toLowerCase();
+            
+            let newURL = `https://dog.ceo/api/breed/${breedParameter}/images/random`;
 
-   // Add or remove active class on button group
-    $('button.active').not(this).removeClass('active');
-     $(this).addClass('active');
-});
+            fetch(newURL)
+                .then(response => response.json())
+                .then(data => 
+                    displayResults(data))
+        }
+
+        function displayResults(data) {
+
+            let dogData = data.message;
+            let dogStatus = data.status;
+
+            if(dogStatus === 'error') {
+                alert('Breed not found (master breed does not exist). Please try again!');
+                return;
+            } else {
+                $('.results-img').replaceWith(`<img src="${dogData}" class="results-img">`);
+            }
+                    
+            //display the results section
+            $('.results').removeClass('hidden');
+        }
+
+        function watchForm() {
+            $('form').submit(event => {
+                event.preventDefault();
+
+               let userBreedEntry = $('#breed-list-entry').val();
+
+                if(userBreedEntry === '') {
+                    alert('Input can not be left blank');
+                    return;
+                }
+
+                getDogImage(userBreedEntry);
+                $('#dog-form').find('input:text').val(''); 
+
+            });
+        }
+
+        $(function() {
+            console.log('App loaded! Waiting for submit!');
+            watchForm();
+        });
+        
+    })
+);
